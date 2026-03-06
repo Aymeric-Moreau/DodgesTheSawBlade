b1a6bfab0220fcf2a9a53a7213d1cda97a6c8da8
Aymeric Moreau <129259587+Aymeric-Moreau@users.noreply.github.com>
2026-03-06 13:45:25 +0100

    Corection partie physique

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,13 @@ Vector2 operator+(Vector2 a, const Vector2 b)
     return a;
 }
 
+Vector2 operator*(Vector2 a, const float b)
+{
+    a.x *= b;
+    a.y *= b;
+    return a;
+}
+
 constexpr Color BACKGROUND = BLUE;
 constexpr Color CHARACTER = PURPLE;
 constexpr int TAILLECHARACTER = 20;
@@ -32,9 +39,9 @@ constexpr int SCREENHEIGHT = 990;
 int characterX = 10;
 int characterY = 12;
 
-int speed = 7;
-int jumpHeigh = 200;
-Vector2 jumpSpeed = {0,25};
+int speed = 300;
+int jumpHeigh = 20;
+Vector2 jumpSpeed = {0,550};
 Vector2 slowdowGravityAfterFallFromJump = {0,1};
 int playerheightBeforeJump;
 float slowdownDuration = 0.5;
@@ -42,7 +49,7 @@ float slowdownDuration = 0.5;
 float timerSlowDown = 0.0f;
 
 
-Vector2 gravity = {0,10};
+Vector2 gravity = {0,100};
 
 Vector2 velocity = gravity;
 Vector2 playerPos = {10 , 12};
@@ -53,6 +60,7 @@ bool startFallFromJump = false;
 
 Camera2D camera;
 Rectangle rect = { -2500, 200, 5000, 10 };
+float deltaTime;
 
 int main()
 {
@@ -72,15 +80,22 @@ int main()
 
     while (!WindowShouldClose())
     {
-        velocity = gravity;
+        
+        deltaTime = GetFrameTime();
+
+        if (!grounded)
+        {
+            velocity = gravity*deltaTime;
+        }
+        
         if (inJump)
         {
-            velocity -= jumpSpeed;
+            velocity -= jumpSpeed * deltaTime;
         }
 
         if (startFallFromJump)
         {
-            velocity -= slowdowGravityAfterFallFromJump;
+            velocity -= slowdowGravityAfterFallFromJump * deltaTime;
         }
 
         
@@ -89,9 +104,9 @@ int main()
         {
             if (grounded)
             {
-                velocity.y =- gravity.y;
+                velocity.y =0;
             }
-            velocity.x =+ speed;
+            velocity.x =+ speed * deltaTime;
             // characterX+= speed;
         }
 
@@ -99,10 +114,10 @@ int main()
         {
             if (grounded)
             {
-                velocity.y =- gravity.y;
+                velocity.y = 0;
             }
             
-            velocity.x =- speed;
+            velocity.x =- speed * deltaTime;
             // characterX-= speed;
         }
 
@@ -120,7 +135,7 @@ int main()
 
         if (IsKeyDown(KEY_SPACE) && grounded) // il faut que ce soit une velociter constante pendant un certain temp
         {
-            playerheightBeforeJump = playerPos.y;
+            playerheightBeforeJump = playerPos.y * deltaTime;
             inJump = true;
             // velocity.y = gravity.y - jumpForce;
             // characterY-= speed; // Monter
@@ -194,9 +209,10 @@ int main()
         std::string veloY = std::to_string(velocity.y);
         std::string fullTextCo = "Position : X  : " + coX + " Y : " + coY + " Veclocity : X : " + veloX + " Y : " + veloY;
 
+        std::string GroundedStatus = grounded ? "grounded" : "in air";
         std::string JumpStatus = inJump ? "en jump" : "Pas en jump";
         std::string overlapStatus = r ? "Overlap" : "not overlap";
-        std::string fullText = "Print  : " + overlapStatus + " jump = " + JumpStatus;
+        std::string fullText = "Print  : " + overlapStatus + " jump = " + JumpStatus + " grounded : " + GroundedStatus;
         DrawText(fullText.c_str(), 10, 10, 30, RED);
         DrawText(fullTextCo.c_str(), 10, 50, 10, YELLOW);
 
